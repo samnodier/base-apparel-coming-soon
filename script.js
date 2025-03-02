3f255a9ad96411b0abee561bbbd7ada937732c77
samnodier <sam0132nodier@gmail.com>
2025-03-02 13:52:57 -0500

    final commit

--- script.js
+++ script.js
@@ -6,19 +6,32 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Stop the default form submission
-
-    if (!emailRegex.test(email.value)) {
+    if (email.value.trim()==="") {
+        email.classList.remove("error");
+        errorIcon.classList.add("hidden");
+        errorMessage.classList.add("hidden");
+    }
+    else if (!emailRegex.test(email.value)) {
         errorIcon.classList.remove("hidden");
         errorMessage.classList.remove("hidden"); // Show error message
+        email.classList.add("error"); // Make the border red
+
     }
 })
 
 email.addEventListener("input", function(event) {
-    if (!emailRegex.test(email.value)) {
+    if (email.value.trim()==="") {
+        email.classList.remove("error");
+        errorIcon.classList.add("hidden");
+        errorMessage.classList.add("hidden");
+    }
+    else if (!emailRegex.test(email.value)) {
         errorIcon.classList.remove("hidden");
         errorMessage.classList.remove("hidden"); // Show error message
+        email.classList.add("error");
     } else {
         errorIcon.classList.add("hidden");
         errorMessage.classList.add("hidden"); // Show error message
+        email.classList.remove("error");
     }
 })
